affe95f2ccca22174c3ec799a31d09b14d030fbe
katsuma0118 <67643761+katsuma0118@users.noreply.github.com>
2025-03-18 13:27:09 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import MemberChat from './MemberChat';
 
-import './index.css';
-
 ReactDOM.createRoot(document.getElementById('root')).render(<MemberChat />);
